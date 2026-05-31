25a217c53fd0bcb2b64f761d76a90800f30bb26e
Vinicius Manfrin <vinizamara2@gmail.com>
2026-05-30 22:03:17 -0300

    build: configuração inicial do ambiente da API

---
+++ BusinessManagementApi/src/main/java/com/thegroup/business_management_api/BusinessManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.thegroup.business_management_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BusinessManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusinessManagementApiApplication.class, args);
+	}
+
+}
---
+++ BusinessManagementApi/src/test/java/com/thegroup/business_management_api/BusinessManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thegroup.business_management_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessManagementApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
